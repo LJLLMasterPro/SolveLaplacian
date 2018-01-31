637ef9875f5f29a83d286c30e81c2930346b08ab
Invité <juvigny@onera.fr>
2018-01-31 08:09:24 +0100

    Changes on shell installation script and environment variable script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 (cd thirdparty/metis-5.1.0; make config shared=1 prefix=`pwd`/../..; make; make install)
-(cd Modules; python setup.py install --home=`pwd`)
+(cd Modules; python setup.py install --home=`pwd`/..)
 
--- varsPython.sh
+++ varsPython.sh
@@ -1,2 +1,2 @@
-export PYTHONPATH=~/lib/python/Project:~/lib64/python/Project:/usr/lib64/python2.7/site-packages/openmpi
+export PYTHONPATH=`pwd`/lib/python/Project:`pwd`/lib64/python/Project:/usr/lib64/python2.7/site-packages/openmpi
 export LD_LIBRARY_PATH=./lib:$LD_LIBRARY_PATH
